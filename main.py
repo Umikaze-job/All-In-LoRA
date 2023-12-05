f0490768ab01c060a7cda47a8ed828f72dd7c13a
Umikaze-job <umikaze@blackgopher468.sakura.ne.jp>
2023-12-05 09:44:44 +0900

    main

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-from fastapi import FastAPI, Request
-from fastapi.staticfiles import StaticFiles
+from fastapi import FastAPI, Request, File, UploadFile, Form
+from fastapi.datastructures import Headers
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.params import Header
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 from modules.folder_select import Folder_Select
 
 app = FastAPI()
 
+class MyStatics(StaticFiles):
+    def is_not_modified(self, response_headers: Header, request_headers: Headers) -> bool:
+        # your own cache rules goes here...
+        return False
+
 # "assets"というディレクトリに静的ファイルが存在すると仮定
-app.mount("/assets", StaticFiles(directory="assets"), name="assets")
+app.mount("/assets", app=StaticFiles(directory="assets"), name="assets")
+
+# 静的コンテンツのエンドポイント
+app.mount("/savefiles", app=MyStatics(directory="savefiles"), name="savefiles")
 
 origins = [
     "http://localhost:64383",
@@ -32,6 +43,7 @@ templates = Jinja2Templates(directory="templates")
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
+# folder-select関係
 @app.post("/api/folder-select/get-folders")
 async def Folder_Select_Get_Folders(request: Request):
     return await Folder_Select.Get_Folders(request)
@@ -39,3 +51,11 @@ async def Folder_Select_Get_Folders(request: Request):
 @app.post("/api/folder-select/create")
 async def Folder_Select_Create(request: Request):
     return await Folder_Select.Create(request)
+
+@app.post("/api/folder-select/rename")
+async def Folder_Select_Rename(request: Request):
+    return await Folder_Select.Rename(request)
+
+@app.post("/api/folder-select/thumbnail")
+async def Folder_Select_Thumbnail(folderName: str = Form(), image: UploadFile = Form()):
+    return await Folder_Select.Thumbnail(folderName,image)
--- modules/folder_select.py
+++ modules/folder_select.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, File, UploadFile, Form
+from fastapi.responses import JSONResponse
 import asyncio
 import os
+import shutil
 
 from pydantic import BaseModel
 
@@ -13,19 +15,66 @@ def get_root_folder_path():
 
     return root_folder_path
 
-def create_folder(folder_path):
-    try:
-        os.makedirs(folder_path, exist_ok=True)
-        print(f"フォルダ '{folder_path}' を作成しました。")
-    except OSError as e:
-        print(f"フォルダの作成中にエラーが発生しました: {e}")
+# 指定したフォルダの中にサムネイルを作成する
+def make_thumbnail(target_folder:str):
+    # ソースフォルダとターゲットフォルダのパスを指定
+    source_folder = os.path.join(get_root_folder_path(),"assets")
+
+    # ソースフォルダ内のファイルリストを取得
+    files = os.listdir(source_folder)
+
+    # 'thumbnail_pre.png' を見つけてコピー
+    for file in files:
+        if file == 'thumbnail_pre.png': #! 'thumbnail_pre.png'という画像を元にサムネイルを生成
+            source_path = os.path.join(source_folder, file)
+            target_path = os.path.join(target_folder, 'thumbnail.png')
+            shutil.copyfile(source_path, target_path)
+
+# 各フォルダの中にあるthumbnail.pngのパスを集めた配列
+def get_thumbnail_arr(folder_paths:[]):
+    # 'thumbnail.png' を探してパスを収集するための空のリスト
+    thumbnail_paths = []
+
+    # 各フォルダ内の 'thumbnail.png' を探す
+    for folder_path in folder_paths:
+        thumbnail_path = os.path.join(folder_path, 'thumbnail.png')
+        
+        # 'thumbnail.png' が存在する場合、リストに追加
+        if os.path.exists(thumbnail_path):
+            thumbnail_paths.append(thumbnail_path)
+
+    return thumbnail_paths
+
+# ファイル名の配列からファイルパスが入っている配列を作成する。
+def get_folder_paths_from_savefiles(filenames:[]):
+
+    # 各要素に対して os.path.join(get_root_folder_path(), 要素) を適用する
+    
+    return [os.path.join(f"http://localhost:8000","savefiles", s,"thumbnail.png") for s in filenames]
+
+# 指定したフォルダの中に指定した名前のフォルダがあるかどうか
+def is_folder_exists(parent_folder, target_folder_name):
+    # 指定したフォルダ内のすべてのフォルダを取得
+    all_folders = [f for f in os.listdir(parent_folder) if os.path.isdir(os.path.join(parent_folder, f))]
+
+    # 指定した名前のフォルダが存在するか確認
+    return target_folder_name in all_folders
 
 class Folder_Select:
+    # フォルダ作成
     async def Create(request:Request):
         data = await request.json()
         folder_name = data.get('name')
-        create_folder(os.path.join(get_root_folder_path(),"savefiles",folder_name))
-        return {"message": "OK"}
+        folder_path = os.path.join(get_root_folder_path(),"savefiles",folder_name)
+        if is_folder_exists(os.path.join(get_root_folder_path(),"savefiles"),folder_name):
+            return {"message":"Duplicate names"}
+        else:
+            try:
+                os.makedirs(folder_path, exist_ok=True)
+                make_thumbnail(folder_path)
+                return {"message": "Folder Created!!!"}
+            except OSError as e:
+                return {"message": "Error"}
     
     async def Get_Folders(request:Request):
         dir_path = os.path.join(get_root_folder_path(),"savefiles")
@@ -33,4 +82,38 @@ class Folder_Select:
 
         # 更新日時でソート
         directories_sorted = sorted(directories, key=lambda f: os.path.getmtime(os.path.join(dir_path, f)), reverse=True)
-        return directories_sorted
+        return {"directoriesName":directories_sorted,"thumbnail":get_folder_paths_from_savefiles(directories_sorted)}
+    
+    # 名前変更
+    async def Rename(request:Request):
+        data = await request.json()
+        before_name = data.get('beforeName')
+        after_name = data.get('afterName')
+        if is_folder_exists(os.path.join(get_root_folder_path(),"savefiles"), after_name):
+            return {"error":"Duplicate names"}
+        else:
+            try:
+                old_path = os.path.join(get_root_folder_path(),"savefiles",before_name)
+                # 新しい名前のパスを生成
+                new_path = os.path.join(get_root_folder_path(),"savefiles",after_name)
+                
+                # フォルダの名前を変更
+                os.rename(old_path, new_path)
+                return {"message": "ok"}
+            except FileNotFoundError:
+                return {"error":"file not found"}
+            except Exception as e:
+                return {"error":"some error"}
+            
+    # サムネイル設定
+    async def Thumbnail(folderName: str = Form(), image: UploadFile = Form()):
+        try:
+            print(folderName)
+            print(image)
+            # ファイルを指定したフォルダに保存
+            save_path = os.path.join(get_root_folder_path(),"savefiles",folderName, "thumbnail.png")
+            with open(save_path, "wb") as buffer:
+                shutil.copyfileobj(image.file, buffer)
+            return {"message":"Thumbnails have been set up!!!!"}
+        except Exception as e:
+            return JSONResponse(content={"message": f"Error uploading file: {e}"}, status_code=500)
