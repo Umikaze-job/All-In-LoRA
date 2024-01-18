4425025778a52bcdc6e04a0c989c9744274b1261
Umikaze-job <umikaze@blackgopher468.sakura.ne.jp>
2024-01-18 20:25:50 +0900

    change

--- shell_command/real_esrgan_install.bat
+++
@@ -1,2 +0,0 @@
-call cd D:\ai\My_AI\dataset_edit\All-In-Lora\All-In-Lora\tools
-call git clone https://github.com/xinntao/Real-ESRGAN
--- shell_command/real_esrgan_resize.bat
+++
@@ -1,5 +0,0 @@
-call cd D:\ai\My_AI\dataset_edit\All-In-Lora\All-In-Lora 
-call .\venv\Scripts\activate 
-call cd D:\ai\My_AI\dataset_edit\All-In-Lora\All-In-Lora\tools\Real-ESRGAN
-call python inference_realesrgan.py -i D:\ai\My_AI\dataset_edit\All-In-Lora\All-In-Lora\savefiles\data02\character_trimming_folder\temp\derestefeet01_face000.webp -o D:\ai\My_AI\dataset_edit\All-In-Lora\All-In-Lora\savefiles\data02\character_trimming_folder\temp -n RealESRGAN_x4plus_anime_6B -s 2
-pause
